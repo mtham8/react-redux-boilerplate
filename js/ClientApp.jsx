76257fb46b65bdd29dc8a935a299a344b4749046
Michelle Tham <mtham101@gmail.com>
2016-11-28 14:47:23 -0800

    Remove comments

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,7 +3,6 @@ const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const { Router, Route, hashHistory } = require('react-router')
 
-// you need div to be a parent element for the instances of your class
 const App = () => (
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
