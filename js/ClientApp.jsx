42a24b316f31f4139646897faafa0203129230da
Michelle Tham <mtham101@gmail.com>
2016-11-28 16:50:46 -0800

    Implement nest routes

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,12 +2,15 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Component1 = require('./Component1')
-const { Router, Route, hashHistory } = require('react-router')
+const Layout = require('./Layout')
+const { Router, Route, hashHistory, IndexRoute } = require('react-router')
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/component1' component={Component1} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/component1' component={Component1} />
+    </Route>
   </Router>
 )
 
