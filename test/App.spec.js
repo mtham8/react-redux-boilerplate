42e421988f746f910be0157bf3da6b43be06eacf
Michelle Tham <mtham101@gmail.com>
2016-11-28 20:38:30 -0800

    Implement test for Component1

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,13 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Component1 = require('../js/Component1')
+const { shallow } = require('enzyme')
 
-describe('<Landing /> ', () => {
-  it('should pass', () => {
-    expect(1 + 1 === 2).to.be.true
+describe('<Component1 /> ', () => {
+  it('should render the title', () => {
+    const wrapper = shallow(<Component1 />)
+    expect(wrapper.contains(<h1>This is Component 1</h1>)).to.be.true
   })
 })
