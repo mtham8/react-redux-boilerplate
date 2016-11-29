7ac7d78dac12b1249764f0b780beec27ff3665fd
Michelle Tham <mtham101@gmail.com>
2016-11-28 16:53:45 -0800

    Remove excess app-container in Landing component

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 const { Link } = require('react-router')
 
 const Landing = () => (
-  <div className='app-container'>
+  <div>
     <h1>Landing Component</h1>
     <Link to='/component1' className='nav-button'> Go to Component1 </Link>
   </div>
